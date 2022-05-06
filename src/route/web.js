c0bf9b3521d1b9c2959bbb4c3742ade3ed035718
Quang <dq08042000@gmail.com>
2022-05-06 10:38:40 +0700

    commit

--- src/route/web.js
+++ src/route/web.js
@@ -1,13 +1,16 @@
 import express from 'express'
-import {CRUDuser,postCrud ,getHomePage} from '../controller/homeController'
+import {CRUDuser,postCrud ,getHomePage,displayUser,editCrud,putCrud,deleteCrud} from '../controller/homeController'
 const router = express.Router()
 
 const initWebRoutes = (app) => {
     router.get('/',getHomePage)
     router.get('/cruduser',CRUDuser)
     router.post('/post-crud',postCrud )
-    // router.get('/get-user',displayUser)
-    
+    router.get('/get-user',displayUser)
+    router.get('/edit-crud',editCrud)
+    router.post('/put-crud',putCrud)
+    router.get('/delete-crud',deleteCrud)
+      
 
     return app.use("/",router)
 }
--- src/services/services.js
+++ src/services/services.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs'
 import db from '../models/index';
 
 
+
 var salt = bcrypt.genSaltSync(10);
 const createNewUser = async(data) =>{
     return new Promise(async (resolve, reject) => {
@@ -38,21 +39,94 @@ const hashUserPassWork =(password) => {
        }
     })
 }
+const getDataUser = ()=>{
+    return new Promise (async (resolve,reject)=>{
+        try{
+            const dataUser =await db.User.findAll({
+                raw:true
+            })
+            resolve(dataUser)
 
-// const getAllUser = ()=>{
-//     return new Promise( async (resolve,reject)=>{
-//      try {
-//         const users = await  db.User().findAll()
-//         resolve(users)
-//      }catch(e){
-//          reject(e)
-//      }
-//     })
-// }
+        }catch(e){
+            reject(e)
+        }
+
+})
+}
+const editUserById = (idUser) =>{
+    return new Promise (async (resolve, reject)=>{
+        try{
+        
+            const dataUserId =await db.User.findOne({
+                where:{id:idUser},
+                raw:true
+            })
+            
+         if(dataUserId){
+            resolve(dataUserId)
+         }else{
+             resolve({})
+         }
+          
+        }catch(e){
+            reject(e)
+        }
+    })
+}
+const putCRUD = (dataPut) =>{
+    return new Promise(async (resolve,reject)=>{
+        try{
+            const updateId =await db.User.findOne({
+                where:{id:dataPut.id}
+            })
+
+            if(updateId){
+             updateId.firstName = dataPut.firstName
+             updateId.lastName = dataPut.lastName
+             updateId.address= dataPut.address
+             updateId.phonenumber=dataPut.phonenumber
+             updateId.gender = dataPut.gender
+             await updateId.save()
+             const getUserAll = db.User.findAll({
+                raw:true 
+             })
+             resolve(getUserAll)
+            }else{
+                resolve()
+            }
+        }catch(e){
+            reject(e)
+        }
+    })
+}
+
+const deleteCRUD = (iddata)=>{
+    return new Promise (async(resolve,reject)=>{
+        try{
+
+            const idDelete =await db.User.findOne({
+                where:{id:iddata}
+            })
+            if(idDelete){
+               await idDelete.destroy()
+                resolve()
+            }else{
+                resolve({})
+            }
+        
+        }catch(e){
+            reject(e)
+        }
+    })
+}
 
 module.exports= {
 
     createNewUse:createNewUser,
-    // getAllUser:getAllUser
+    getDataUser:getDataUser,
+    editUserById:editUserById,
+    putCRUD:putCRUD,
+    deleteCRUD:deleteCRUD
+   
     
 }
